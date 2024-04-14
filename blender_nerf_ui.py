e522822ab7050684f5e87545aadf112c3fdb7947
chroniclesoftintin <mkrsiscar@firstphilec.com>
2024-04-15 07:52:48 +0800

    initial

---
+++ blender_nerf_ui.py
@@ -0,0 +1,43 @@
+import bpy
+
+
+# blender nerf shared ui properties class
+class DepthSculptNERF_UI(bpy.types.Panel):
+    '''DepthSculptNERF UI'''
+    bl_idname = 'VIEW3D_PT_blender_nerf_ui'
+    bl_label = 'DepthSculptNERF shared UI'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'DepthSculptNERF'
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+
+        layout.alignment = 'CENTER'
+
+        row = layout.row(align=True)
+        row.prop(scene, 'train_data', toggle=True)
+        row.prop(scene, 'test_data', toggle=True)
+
+        if not (scene.train_data or scene.test_data):
+            layout.label(text='Nothing will happen!')
+
+        else:
+            layout.prop(scene, 'aabb')
+
+            if scene.train_data:
+                layout.separator()
+                layout.prop(scene, 'render_frames')
+
+            layout.prop(scene, 'logs')
+            layout.separator()
+            layout.label(text='File Format')
+
+            row = layout.row(align=True)
+            row.prop(scene, 'nerf', toggle=True, text='NGP', invert_checkbox=True)
+            row.prop(scene, 'nerf', toggle=True)
+
+            layout.separator()
+            layout.use_property_split = True
+            layout.prop(scene, 'save_path')
